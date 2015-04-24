77a8d4287d89434ab8c2496116099d3c6efc15b9
jason_lee <jason979708642@gmail.com>
2015-04-24 17:17:42 +0800

    0.1

--- role/models.py
+++ role/models.py
@@ -1,14 +1,18 @@
 #coding=utf-8
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Group
 from django.utils.translation import gettext as _
 # Create your models here.
 from simple_history.models import HistoricalRecords
-
+from order.models import *
+from django.db.models import *
+import datetime
+from django.contrib.auth.models import User
 #job_choices = (
 #    (0, _("service")),
 #    (1, _("Storekeeper")),
 #)
+
 class UserProfile(models.Model):
     '''
     用户表
@@ -41,3 +45,251 @@ class Issuing_person(models.Model):
     def __unicode__(self):
         return '%s'%self.name
         
+class User_goal(models.Model):
+    user=models.ForeignKey(User,verbose_name='用户')
+    goal_quantity=models.IntegerField(verbose_name="目标量",null=True,blank=True)
+    goal_money=models.DecimalField(verbose_name="目标成交额",max_digits=7, decimal_places=2,default=0,null=True,blank=True)
+    goal_chosen=models.BooleanField(verbose_name="0:出单量,1:目标额",default=0)
+
+
+    def __unincode__(self):
+        if self.goal_chosen:
+            return '%s-%s'%(self.user.first_name,self.goal_money)
+        else:
+            return '%s-%s'%(self.user.first_name,self.goal_quantity)
+
+class Issuing_person_cut(models.Model):
+    issuing_person=models.ForeignKey(Issuing_person,verbose_name="出单人")
+    cut_percentage=models.DecimalField(verbose_name="提成系数",max_digits=2, decimal_places=2,default=0,null=True,blank=True)
+    jointime=models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '%s-%s'%(self.issuing_person,self.cut_percentage)
+
+class Server_cut(models.Model):
+    server=models.ForeignKey(User,verbose_name="客服")
+    cut_percentage=models.DecimalField(verbose_name="提成系数",max_digits=2, decimal_places=2,default=0,null=True,blank=True)
+    jointime=models.DateTimeField(auto_now=True)
+    def __unicode__(self):
+        return '%s-%s'%(self.server,self.cut_percentage)
+
+
+def get_group_name(self):
+    try:
+        name=self.groups.all()[0].name
+    except:
+        name=''
+    return name
+
+def get_this_today(self):
+    today=datetime.date.today()
+    if self.is_superuser:
+        today_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__is_superuser=False))&Q(jointime__gte=today))
+    else:
+        if self.id in [i[0] for i in Customer.objects.filter(user__isnull=False).values_list("user").distinct()]:
+            today_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user=self))&Q(jointime__gte=today))
+        else:
+            return (0,0)
+    this_day_quantity=today_order.count()
+    this_day_money=sum([i.product.price for i in today_order])
+    return (this_day_money,this_day_quantity)
+def get_this_week(self):
+    week_monday=datetime.date.today()-datetime.timedelta(days=datetime.date.today().weekday())
+    if self.is_superuser:
+        this_week_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__is_superuser=False))&Q(jointime__gte=week_monday))
+    else:
+        if self.id in [i[0] for i in Customer.objects.filter(user__isnull=False).values_list("user").distinct()]:
+            this_week_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user=self))&Q(jointime__gte=week_monday))
+        else:
+            return (0,0)
+    this_week_quantity=this_week_order.count()
+    this_week_money=sum([i.product.price for i in this_week_order])
+    return (this_week_money,this_week_quantity)
+def get_this_month(self):
+    this_month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    if self.is_superuser:
+        this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__is_superuser=False))&Q(jointime__gte=this_month_start))
+    else:
+        if self.id in [i[0] for i in Customer.objects.filter(user__isnull=False).values_list("user").distinct()]:
+            this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user=self))&Q(jointime__gte=this_month_start))
+        else:
+            return (0,0)
+    this_month_quantity=this_month_order.count()
+    this_month_money=sum([i.product.price for i in this_month_order])
+    return (this_month_money,this_month_quantity)
+
+def get_person_goal(self):
+    try:
+        goal=User_goal.objects.get(user=self)
+    except:
+        goal=0
+    return goal
+
+
+def get_person_cut(self):
+    try:
+        cut=Server_cut.objects.get(server=self)
+    except:
+        cut=0
+    return cut
+def get_order_success(self):
+    this_month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    if self.is_superuser:
+        this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__is_superuser=False))&Q(jointime__gte=this_month_start)).count()
+        this_month_success=Order.objects.filter(Q(customer__in=Customer.objects.filter(
+            user__is_superuser=False))&Q(jointime__gte=this_month_start)&Q(state=Order_State.objects.get(name=_('已签收')))).count()
+    else:
+        this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user=self))&Q(jointime__gte=this_month_start)).count()
+        this_month_success=Order.objects.filter(Q(customer__in=Customer.objects.filter(
+            user=self))&Q(jointime__gte=this_month_start)&Q(state=Order_State.objects.get(name=_('已签收')))).count()
+    try:
+        success_per="%.2f%%" %((float(this_month_success) / float(this_month_order)) * 100)
+    except:
+        success_per=0
+    return success_per
+
+def get_team_goal(self):
+    team_user=self.groups.all()[0].user_set.all()
+    goal_money=User_goal.objects.filter(user__in=team_user).aggregate(Sum('goal_money'))['goal_money__sum']
+    return goal_money
+
+def get_team_order(self):
+    this_month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    team_user=self.groups.all()[0].user_set.all()
+    team_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__in=team_user))&Q(jointime__gte=this_month_start))
+    team_month_money=sum([i.product.price for i in team_month_order])
+    return team_month_money
+
+def get_team_order_success(self):
+    team_user=self.groups.all()[0].user_set.all()
+    this_month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__in=team_user))&Q(jointime__gte=this_month_start)).count()
+    this_month_success=Order.objects.filter(Q(customer__in=Customer.objects.filter(
+            user=team_user))&Q(jointime__gte=this_month_start)&Q(state=Order_State.objects.get(name=_('已签收')))).count()
+    try:
+        success_per="%.2f%%" %((float(this_month_success) / float(this_month_order)) * 100)
+    except:
+        success_per=0
+    return success_per
+
+def this_day_rank(self):
+    today=datetime.date.today()
+    team_user=self.groups.all()[0].user_set.all()
+    rank_list=[]
+    for i in team_user:
+        this_day_order=Order.objects.filter(Q(
+        customer__in=Customer.objects.filter(user=i))&Q(
+        jointime__gte=today))
+        if this_day_order:
+            this_day_order=this_day_order.aggregate(sum_order=Sum("product__price"))
+            rank_list.append((i,this_day_order['sum_order']))
+        else:
+            rank_list.append((i,0))
+
+    rank_list=sorted(rank_list,key=(lambda x:x[1]),reverse=True)
+    for i in rank_list:
+        if self in i:
+            return rank_list.index(i)+1,i[1]
+
+def this_week_rank(self):
+    week_monday=datetime.date.today()-datetime.timedelta(days=datetime.date.today().weekday())
+    team_user=self.groups.all()[0].user_set.all()
+    rank_list=[]
+    for i in team_user:
+        this_week_order=Order.objects.filter(Q(
+        customer__in=Customer.objects.filter(user=i))&Q(
+        jointime__gte=week_monday))
+        if this_week_order:
+            this_week_order=this_week_order.aggregate(sum_order=Sum("product__price"))
+            rank_list.append((i,this_week_order['sum_order']))
+        else:
+            rank_list.append((i,0))
+    rank_list=sorted(rank_list,key=(lambda x:x[1]),reverse=True)
+    for i in rank_list:
+        if self in i:
+            return rank_list.index(i)+1,i[1]
+        
+def this_month_rank(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    team_user=self.groups.all()[0].user_set.all()
+    rank_list=[]
+    for i in team_user:
+        this_month_order=Order.objects.filter(Q(
+        customer__in=Customer.objects.filter(user=i))&Q(
+        jointime__gte=month_start))
+        if this_month_order:
+            this_month_order=this_month_order.aggregate(sum_order=Sum("product__price"))
+            rank_list.append((i,this_month_order['sum_order']))
+        else:
+            rank_list.append((i,0))
+    rank_list=sorted(rank_list,key=(lambda x:x[1]),reverse=True)
+    for i in rank_list:
+        if self in i:
+            return rank_list.index(i)+1,i[1]
+
+def person_quantity_rank(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    this_month_order=Order.objects.filter(Q(
+        jointime__gte=month_start))
+    quantity_person=this_month_order.values("customer__user").annotate(quantity=Count("customer__user")).order_by("-quantity")
+    quantity_person=quantity_person.exclude(customer__user=None)[:5]
+    for i in quantity_person:
+        if i['customer__user']:
+            i['customer__user']=User.objects.get(id=i['customer__user'])
+    return quantity_person
+
+def person_money_rank(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    this_month_order=Order.objects.filter(Q(
+        jointime__gte=month_start))
+
+    money_person=this_month_order.values("customer__user").annotate(money=Sum("product__price")).order_by("-money")
+    money_person=money_person.exclude(customer__user=None)[:5]
+    for i in money_person:
+        if i['customer__user']:
+            i['customer__user']=User.objects.get(id=i['customer__user'])
+    return money_person
+
+def get_team_rank(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    groups=Group.objects.all()
+    groups_list=[]
+    for i in groups:
+        this_month_order=Order.objects.filter(Q(customer__in=Customer.objects.filter(user__in=i.user_set.all()))&Q(jointime__gte=month_start))
+        if this_month_order:
+            total_money=this_month_order.aggregate(money=Sum("product__price"))['money']
+            groups_list.append({"name":i.name,"money":total_money})
+
+    groups_list=sorted(groups_list,key=(lambda x:x['money']),reverse=True)
+    return groups_list
+
+def get_failed_order(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    this_month_order=Order.objects.filter(Q(state=Order_State.objects.filter(~Q(name="已签收")&Q(level__gte=3)))&Q(jointime__gte=month_start)).order_by("-jointime")
+    return this_month_order
+
+def get_no_approval_order(self):
+    month_start=datetime.date(datetime.date.today().year,datetime.date.today().month,1)
+    this_month_order=Order.objects.filter(Q(state=Order_State.objects.filter(level=1))&Q(jointime__gte=month_start)).order_by("-jointime")
+    return this_month_order
+
+
+
+User.add_to_class("get_group_name",get_group_name)
+User.add_to_class("get_this_today",get_this_today)
+User.add_to_class("get_this_week",get_this_week)
+User.add_to_class("get_this_month",get_this_month)
+User.add_to_class("get_person_goal",get_person_goal)
+User.add_to_class("get_person_cut",get_person_cut)
+User.add_to_class("get_order_success",get_order_success)
+User.add_to_class("get_team_goal",get_team_goal)
+User.add_to_class("get_team_order",get_team_order)
+User.add_to_class("get_team_order_success",get_team_order_success)
+User.add_to_class("this_day_rank",this_day_rank)
+User.add_to_class("this_week_rank",this_week_rank)
+User.add_to_class("this_month_rank",this_month_rank)
+User.add_to_class("person_quantity_rank",person_quantity_rank)
+User.add_to_class("person_money_rank",person_money_rank)
+User.add_to_class("get_team_rank",get_team_rank)
+User.add_to_class("get_failed_order",get_failed_order)
+User.add_to_class("get_no_approval_order",get_no_approval_order)
--- role/views.py
+++ role/views.py
@@ -6,22 +6,24 @@ from django.contrib.auth.views import login_required
 from forms import *
 from django.contrib.auth.models import User,Group,Permission,ContentType
 from django.contrib.auth import authenticate,login as auth_login,logout as auth_logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponseForbidden
 from django.contrib.auth.decorators import user_passes_test,login_required
 from django.contrib.auth.forms import UserCreationForm
 from stock.models import *
 from role.models import *
 from order.models import *
 import json
+import datetime
 
 
 @login_required(login_url="/login")
 def home(request):
-    #if request.user.is_superuser:
-
     return render(request,'home.html')
 def permission_deny(request):
-    return HttpResponse("<script language='javascript'>alert('没有权限');history.go(-1)</script>")
+    if request.is_ajax():
+        raise HttpResponseForbidden
+    else:
+        return HttpResponse("<script language='javascript'>alert('没有权限');history.go(-1)</script>")
 def login_validate(request,username,password):
     rtvalue = False
     user = authenticate(username=username,password=password)
@@ -104,10 +106,10 @@ def user_add(request):
             user.save()
             groups=[request.POST.get("group")]
             user.groups=groups
-            response.write("<script language='javascript'>alert('添加成功!');window.location.href='/user/info/';</script>")
+            response.write("<script language='javascript'>alert('添加成功!');window.location.href=document.referrer;</script>")
             return response
         else:
-            response.write("<script language='javascript'>alert('添加失败!');window.location.href='/user/info/';</script>")
+            response.write("<script language='javascript'>alert('添加失败!');window.location.href=document.referrer;</script>")
             return response
     # else:
     #     return render(request,'add_role.html',{'userform':form,'role':role})
@@ -118,10 +120,10 @@ def user_remove(request):
     response=HttpResponse()
     user=User.objects.get(pk=user_id)
     if user.is_superuser:
-        response.write("<script language='javascript'>alert('管理员无法删除!');window.location.href='/user/info/';</script>")
+        response.write("<script language='javascript'>alert('管理员无法删除!');window.location.href=document.referrer;</script>")
     else:
         user.delete()
-        response.write("<script language='javascript'>alert('删除成功!');window.location.href='/user/info/';</script>")
+        response.write("<script language='javascript'>alert('删除成功!');window.location.href=document.referrer;</script>")
     return response
 
 @admin_required(login_url='/login')
@@ -153,7 +155,7 @@ def user_group_edit(request,username):
             user=User.objects.get(username=username)
             user.groups=groups
             user.save()
-            response.write("<script language='javascript'>alert('修改成功!');window.location.href=('/user/info/')</script>")
+            response.write("<script language='javascript'>alert('修改成功!');window.location.href=document.referrer;</script>")
             return response
         else:
             response.write("<script language='javascript'>alert('修改失败!请联系管理员');window.history.go(-1)</script>")
@@ -201,10 +203,10 @@ def role_name_add(request):
                 role.save()
             #role_form.save_m2m()
         #role_name.save()
-            response.write("<script language='javascript'>alert('添加成功!');window.location.href=('/role/info')</script>")
+            response.write("<script language='javascript'>alert('添加成功!');window.location.href=document.referrer;</script>")
             return response
         else:
-            response.write("<script language='javascript'>alert('添加失败!');window.location.href=('/role/info')</script>")
+            response.write("<script language='javascript'>alert('添加失败!');window.location.href=document.referrer;</script>")
             return response
     # return render(request,'add_role.html',{'role':role})
 @admin_required(login_url="/login")
@@ -225,7 +227,7 @@ def role_name_edit(request,role_id):
                 permission_list=[int(i) for i in permission_array if i.isdigit()]
                 role.permissions=permission_list
             role.save()
-            response.write("<script language='javascript'>alert('修改成功!');window.location.href=('/role/info/')</script>")
+            response.write("<script language='javascript'>alert('修改成功!');window.location.href=document.referrer;</script>")
             return response
         else:
             response.write("<script language='javascript'>alert('修改失败!名称不能为空');window.history.go(-1)</script>")
@@ -252,7 +254,7 @@ def role_name_remove(request):
     #         object.delete()
     # role_object.delete()
     role_object.delete()
-    response.write("<script language='javascript'>alert('删除成功!');window.location.href='/role/info/';</script>")
+    response.write("<script language='javascript'>alert('删除成功!');window.location.href=document.referrer;</script>")
     return response
 
 def get_all_permissions():
@@ -320,3 +322,166 @@ def get_user_permission(permission_list):
     data_json=json.dumps(data_json)
     #return HttpResponse(str(data_json))
     return data_json
+
+@admin_required(login_url="/login")
+def server_goals_info(request):
+    server_goals=User_goal.objects.all()
+    goal_form=Server_goalForm()
+    user_list=User.objects.filter(is_superuser=False)
+    return render(request,"goal_info.html",{"goals":server_goals,"goal_form":goal_form,"user_list":user_list})
+
+
+
+@admin_required(login_url="/login")
+def server_goals_info_add(request):
+    response=HttpResponse()
+    if request.method=="POST":
+        form=Server_goalForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            user=data['user']
+            goal_quantity=data['goal_quantity']
+            goal_money=data['goal_money']
+            goal_chosen=data['default']
+            try:
+                User_goal.objects.get(user=user)
+                response.write("<script language='javascript'>alert('无法添加,已存在销售目标!');window.location.href=document.referrer;</script>")
+                return response
+            except:
+                goal=User_goal(user=user,goal_quantity=goal_quantity,goal_money=goal_money,goal_chosen=goal_chosen)
+                goal.save()
+            response.write("<script language='javascript'>alert('添加成功!');window.location.href=document.referrer;</script>")
+            return response
+        else:
+            response.write("<script language='javascript'>alert('添加失败!');window.location.href=document.referrer;</script>")
+            return response
+
+@admin_required(login_url='/login')
+def server_goals_info_remove(request):
+    goal_id=request.GET.get('id')
+    response=HttpResponse()
+    goal=User_goal.objects.get(pk=goal_id)
+    goal.delete()
+    response.write("<script language='javascript'>alert('删除成功!');window.location.href=document.referrer;</script>")
+    return response
+
+@admin_required(login_url='/login')
+def server_goals_info_edit(request):
+    if request.method=="POST":
+        #print request.POST
+        name=request.POST.get('name')
+        if name=='user':
+            value=User.objects.get(first_name=request.POST.get("value"))
+        elif name=="goal_chosen":
+            value=eval(request.POST.get("value"))
+        else:
+            value=request.POST.get('value')
+        info_dict={'%s'%name:value}
+        goal=User_goal.objects.filter(pk=request.POST.get("pk"))
+        goal.update(**info_dict)
+        return HttpResponse('success')
+
+@admin_required(login_url="/login")
+def server_cut_info(request):
+    server_cut=Server_cut.objects.all()
+    cut_form=Server_cutForm()
+    user_list=User.objects.filter(is_superuser=False)
+    return render(request,"server_cut_info.html",{"cuts":server_cut,"cut_form":cut_form,"user_list":user_list})
+
+@admin_required(login_url="/login")
+def server_cut_info_add(request):
+    response=HttpResponse()
+    if request.method=="POST":
+        form=Server_cutForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            server=data['server']
+            cut_percentage=data['cut_percentage']
+            try:
+                Server_cut.objects.get(server=server)
+                response.write("<script language='javascript'>alert('无法添加,已有系数!');window.location.href=document.referrer;</script>")
+                return response
+            except:
+                server_cut=Server_cut(server=server,cut_percentage=cut_percentage)
+                server_cut.save()
+            response.write("<script language='javascript'>alert('添加成功!');window.location.href=document.referrer;</script>")
+            return response
+        else:
+            response.write("<script language='javascript'>alert('添加失败!');window.location.href=document.referrer;</script>")
+            return response
+
+@admin_required(login_url='/login')
+def server_cut_info_edit(request):
+    if request.method=="POST":
+        #print request.POST
+        name=request.POST.get('name')
+        if name=='server':
+            value=User.objects.get(first_name=request.POST.get("value"))
+        else:
+            value=request.POST.get('value')
+        info_dict={'%s'%name:value}
+        server_cut=Server_cut.objects.filter(pk=request.POST.get("pk"))
+        server_cut.update(**info_dict)
+        return HttpResponse('success')
+
+@admin_required(login_url='/login')
+def server_cut_info_remove(request):
+    cut_id=request.GET.get('id')
+    response=HttpResponse()
+    server_cut=Server_cut.objects.get(pk=cut_id)
+    server_cut.delete()
+    response.write("<script language='javascript'>alert('删除成功!');window.location.href=document.referrer;</script>")
+    return response
+
+@admin_required(login_url="/login")
+def issuing_person_cut_info(request):
+    issuing_person_cut=Issuing_person_cut.objects.all()
+    cut_form=Issuing_person_cutForm()
+    issuing_person_list=Issuing_person.objects.all()
+    return render(request,"issuing_person_cut_info.html",{"cuts":issuing_person_cut,"cut_form":cut_form,"user_list":issuing_person_list})
+
+@admin_required(login_url="/login")
+def issuing_person_cut_info_add(request):
+    response=HttpResponse()
+    if request.method=="POST":
+        form=Issuing_person_cutForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            issuing_person=data['issuing_person']
+            cut_percentage=data['cut_percentage']
+            try:
+                Issuing_person_cut.objects.get(issuing_person=issuing_person)
+                response.write("<script language='javascript'>alert('无法添加,已有系数!');window.location.href=document.referrer;</script>")
+                return response
+            except:
+                issuing_person_cut=Issuing_person_cut(issuing_person=issuing_person,cut_percentage=cut_percentage)
+                issuing_person_cut.save()
+            response.write("<script language='javascript'>alert('添加成功!');window.location.href=document.referrer;</script>")
+            return response
+        else:
+            response.write("<script language='javascript'>alert('添加失败!');window.location.href=document.referrer;</script>")
+            return response
+
+@admin_required(login_url='/login')
+def issuing_person_cut_info_edit(request):
+    if request.method=="POST":
+        #print request.POST
+        name=request.POST.get('name')
+        if name=='issuing_person':
+            value=Issuing_person.objects.get(name=request.POST.get("value"))
+        else:
+            value=request.POST.get('value')
+        info_dict={'%s'%name:value}
+        issuing_person_cut=Issuing_person_cut.objects.filter(pk=request.POST.get("pk"))
+        issuing_person_cut.update(**info_dict)
+        return HttpResponse('success')
+
+@admin_required(login_url='/login')
+def issuing_person_cut_info_remove(request):
+    cut_id=request.GET.get('id')
+    response=HttpResponse()
+    issuing_person_cut=Issuing_person_cut.objects.get(pk=cut_id)
+    issuing_person_cut.delete()
+    response.write("<script language='javascript'>alert('删除成功!');window.location.href=document.referrer;</script>")
+    return response
+
